fb0d063ce969145b31376da5917d089858f4a203
Abhishek A B <kehshiba.ab@gmail.com>
2023-05-11 12:45:59 +0530

    Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Home from "./components/Home";
 
 function App() {
   return (
-      <h1 className="text-3xl font-bold ">
-        Tailoring Web Project
-      </h1>
+      <Home />
   );
 }
 
