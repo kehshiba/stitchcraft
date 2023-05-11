fc3565ed7b0c483035e4024b1a30667c1e7badfa
Abhishek A B <kehshiba.ab@gmail.com>
2023-05-11 15:17:25 +0530

    routing and new pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from "./components/Home";
-
+import History from "./components/History"
+import {Route, Routes} from "react-router-dom";
 function App() {
   return (
-      <Home />
-  );
+      <Routes>
+        <Route path="/" element={<Home/> } />
+          <Route path={"/history"} element={<History />} />
+      </Routes>
+
+);
 }
 
 export default App;
